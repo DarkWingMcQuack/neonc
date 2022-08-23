0056b122d7228b1948e8ce698895aef678d13a41
Lukas <mail@lukas.contact>
2022-08-23 15:09:44 +0200

    make String ctor public

--- include/ast/expression/String.hpp
+++ include/ast/expression/String.hpp
@@ -8,7 +8,7 @@ namespace ast {
 
 class String : AreaBase
 {
-
+public:
     constexpr String(lexing::TextArea area, std::string_view value) noexcept
         : AreaBase{std::move(area)}, value_(value) {}
 
