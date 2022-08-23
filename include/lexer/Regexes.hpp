2e09a0c14bdda38c4018236dd5f38a0736099b89
Lukas <mail@lukas.contact>
2022-08-23 15:09:23 +0200

    lexer now uses much better regexes

--- include/lexer/Regexes.hpp
+++ include/lexer/Regexes.hpp
@@ -8,19 +8,19 @@ constexpr auto ws_re = ctre::starts_with<"\\s+">;
 constexpr auto nl_re = ctre::starts_with<"\\n+">;
 
 // keywords
-constexpr auto let_re = ctre::starts_with<"let\\s">;
-constexpr auto fun_re = ctre::starts_with<"fun\\s">;
-constexpr auto type_re = ctre::starts_with<"type\\s">;
-constexpr auto typeclass_re = ctre::starts_with<"typeclass\\s">;
-constexpr auto if_re = ctre::starts_with<"if[^a-zA-Z0-9_]">;
-constexpr auto elif_re = ctre::starts_with<"elif[^a-zA-Z0-9_]">;
-constexpr auto else_re = ctre::starts_with<"else[^a-zA-Z0-9_]">;
-constexpr auto while_re = ctre::starts_with<"while[^a-zA-Z0-9_]">;
-constexpr auto for_re = ctre::starts_with<"for[^a-zA-Z0-9_]">;
-constexpr auto self_value_re = ctre::starts_with<"self[^a-zA-Z0-9_]">;
-constexpr auto self_type_re = ctre::starts_with<"Self[^a-zA-Z0-9_]">;
-constexpr auto true_re = ctre::starts_with<"true\\s">;
-constexpr auto false_re = ctre::starts_with<"false\\s">;
+constexpr auto let_re = ctre::starts_with<"let([^a-zA-Z0-9_]|$)">;
+constexpr auto fun_re = ctre::starts_with<"fun([^a-zA-Z0-9_]|$)">;
+constexpr auto type_re = ctre::starts_with<"type([^a-zA-Z0-9_]|$)">;
+constexpr auto typeclass_re = ctre::starts_with<"typeclass([^a-zA-Z0-9_]|$)">;
+constexpr auto if_re = ctre::starts_with<"if([^a-zA-Z0-9_]|$)">;
+constexpr auto elif_re = ctre::starts_with<"elif([^a-zA-Z0-9_]|$)">;
+constexpr auto else_re = ctre::starts_with<"else([^a-zA-Z0-9_]|$)">;
+constexpr auto while_re = ctre::starts_with<"while([^a-zA-Z0-9_]|$)">;
+constexpr auto for_re = ctre::starts_with<"for([^a-zA-Z0-9_]|$)">;
+constexpr auto self_value_re = ctre::starts_with<"self([^a-zA-Z0-9_]|$)">;
+constexpr auto self_type_re = ctre::starts_with<"Self([^a-zA-Z0-9_]|$)">;
+constexpr auto true_re = ctre::starts_with<"true([^a-zA-Z0-9_]|$)">;
+constexpr auto false_re = ctre::starts_with<"false([^a-zA-Z0-9_]|$)">;
 
 // values
 constexpr auto integer_re = ctre::starts_with<"0|[1-9][0-9]*">;
