768b9a919b7b7aaeb274f7956153c144ce511e79
Lukas <mail@lukas.contact>
2022-09-09 00:08:20 +0200

    make namedtype constexpr

--- include/ast/type/NamedType.hpp
+++ include/ast/type/NamedType.hpp
@@ -12,25 +12,25 @@ namespace ast {
 class NamedType : public AreaBase
 {
 public:
-    NamedType(lexing::TextArea area, std::vector<Identifier>&& namespce, Identifier&& type) noexcept
+    constexpr NamedType(lexing::TextArea area, std::vector<Identifier>&& namespce, Identifier&& type) noexcept
         : AreaBase(area),
           namespce_(std::move(namespce)),
           type_(std::move(type)) {}
 
-    auto getNamespace() const noexcept -> const std::vector<Identifier>&
+    constexpr auto getNamespace() const noexcept -> const std::vector<Identifier>&
     {
         return namespce_;
     }
-    auto getNamespace() noexcept -> std::vector<Identifier>&
+    constexpr auto getNamespace() noexcept -> std::vector<Identifier>&
     {
         return namespce_;
     }
 
-    auto getName() const noexcept -> const Identifier&
+    constexpr auto getName() const noexcept -> const Identifier&
     {
         return type_;
     }
-    auto getName() noexcept -> Identifier&
+    constexpr auto getName() noexcept -> Identifier&
     {
         return type_;
     }
