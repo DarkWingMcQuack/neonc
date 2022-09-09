23d2a295822d81dc41797c53112db9024553a571
Lukas <mail@lukas.contact>
2022-09-09 15:44:49 +0200

    added is_specialization_of trait

---
+++ include/common/Traits.hpp
@@ -0,0 +1,26 @@
+
+#pragma once
+
+#include <compare>
+#include <cstdint>
+#include <ctre/ctre.hpp>
+#include <fmt/core.h>
+#include <lexer/Regexes.hpp>
+#include <lexer/TextArea.hpp>
+#include <lexer/Tokens.hpp>
+#include <optional>
+#include <queue>
+#include <string_view>
+#include <vector>
+
+namespace common {
+
+// clang-format off
+template < template <typename...> class Template, typename T >
+struct is_specialization_of : std::false_type {};
+
+template < template <typename...> class Template, typename... Args >
+struct is_specialization_of< Template, Template<Args...> > : std::true_type {};
+// clang-format on
+
+} // namespace common
