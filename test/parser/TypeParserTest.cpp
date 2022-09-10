52260c3bc09411403a251b5ae2cd86caf47e5525
Lukas <mail@lukas.contact>
2022-09-10 16:45:16 +0200

    added some simple type parser test

--- test/parser/TypeParserTest.cpp
+++ test/parser/TypeParserTest.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <lexer/Lexer.hpp>
 #include <parser/Parser.hpp>
+#include <parser/TypeParser.hpp>
 
 #include <gtest/gtest.h>
 
-using parser::Parser;
+using parser::TypeParser;
 
 auto named_type_test(std::string_view text, std::string_view expected_type, auto... other)
 {
@@ -13,22 +14,26 @@ auto named_type_test(std::string_view text, std::string_view expected_type, auto
 
     ast::NamedType expexted{{0, 0}, std::move(ns), std::move(type)};
 
-    auto result = Parser{text}.named_type();
-    ASSERT_TRUE(!!result);
+    lexing::Lexer lexer{text};
+    auto result = TypeParser{text, std::move(lexer)}.type();
 
-    EXPECT_EQ(result.value(), expexted);
+    ASSERT_TRUE(!!result);
+    ASSERT_TRUE(std::holds_alternative<ast::NamedType>(result.value()));
+
+    EXPECT_EQ(std::get<ast::NamedType>(result.value()), expexted);
 }
 
 TEST(TypeParserTest, NamedTypeParserTest)
 {
     named_type_test("hello", "hello");
+    named_type_test("(hello)", "hello");
+    named_type_test("((hello))", "hello");
+    named_type_test("(((hello)))", "hello");
     named_type_test("foo::bar::baz", "baz", "foo", "bar");
+    named_type_test("(foo::bar::baz)", "baz", "foo", "bar");
+    named_type_test("((foo::bar::baz))", "baz", "foo", "bar");
     named_type_test("foo::foo::foo", "foo", "foo", "foo");
     named_type_test("foo::foo::foo", "foo", "foo", "foo");
     named_type_test("a::b::c::d::e::f::g", "g", "a", "b", "c", "d", "e", "f");
-
-	//whitespace not allowed in named_type
-	EXPECT_FALSE(Parser{"foo:: bar::baz"}.named_type());
-	//empty identifier not allowed
-	EXPECT_FALSE(Parser{"foo::::baz"}.named_type());
+    named_type_test("(a::b::c::d::e::f::g)", "g", "a", "b", "c", "d", "e", "f");
 }
