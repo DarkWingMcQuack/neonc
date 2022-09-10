894d55d364169bfacb0c0af00ff1b33f54694d07
Lukas <mail@lukas.contact>
2022-09-10 16:44:43 +0200

    added identifierparser test

---
+++ test/parser/IdentifierParserTest.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <lexer/Lexer.hpp>
+#include <parser/IdentifierParser.hpp>
+
+#include <gtest/gtest.h>
+
+using parser::IdentifierParser;
+
+
+auto identifier_test(std::string_view text, std::string_view expected_value)
+{
+    ast::Identifier expexted{{0, 0}, expected_value};
+
+    lexing::Lexer lexer{text};
+
+    auto result = IdentifierParser{text, std::move(lexer)}.identifier();
+
+    ASSERT_TRUE(!!result);
+
+    EXPECT_EQ(result.value(), expexted);
+}
+
+
+TEST(IdentifierParserTest, SimpleIdentifierParserTest)
+{
+    identifier_test("hello", "hello");
+    identifier_test("hello ", "hello");
+    identifier_test("hello asd", "hello");
+    identifier_test("hs_ello asd", "hs_ello");
+    identifier_test("__ asd", "__");
+}
