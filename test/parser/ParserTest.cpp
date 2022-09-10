7d92316073bca319ec36a083c4849c910513bfb6
Lukas <mail@lukas.contact>
2022-09-10 16:44:57 +0200

    remove parser test

--- test/parser/ParserTest.cpp
+++ test/parser/ParserTest.cpp
@@ -5,24 +5,3 @@
 #include <gtest/gtest.h>
 
 using parser::Parser;
-
-auto simple_id_test(std::string_view text) -> void
-{
-    auto result = Parser{text}.identifier();
-    ASSERT_TRUE(!!result);
-
-    auto id = result.value();
-
-    EXPECT_EQ(id.getValue(), text);
-}
-
-TEST(ParserTest, SimpleIdentifierParsingTest)
-{
-    simple_id_test("asasas");
-    simple_id_test("identifier");
-    simple_id_test("iamanidentifier");
-    simple_id_test("iamanidentifier_");
-    simple_id_test("iamanidentifier_");
-    simple_id_test("iamanidentifier_12");
-    simple_id_test("a12");
-}
