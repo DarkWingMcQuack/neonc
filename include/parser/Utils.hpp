316893a520a16bcf2cd64b70a5965bc11ebfca16
Lukas <mail@lukas.contact>
2022-09-11 11:35:00 +0200

    started writing the expression parser

--- include/parser/Utils.hpp
+++ include/parser/Utils.hpp
@@ -12,25 +12,6 @@ constexpr auto is_digit(char c) noexcept
     return c <= '9' && c >= '0';
 }
 
-
-constexpr auto parse_int(std::string_view str) noexcept
-    -> std::optional<std::int64_t>
-{
-    if(str.empty()) [[unlikely]] {
-        return std::nullopt;
-    }
-
-    std::int64_t value = 0;
-    for(char c : str) {
-        if(!is_digit(c)) [[unlikely]] {
-            return std::nullopt;
-        }
-        value = (c - '0') + value * 10;
-    }
-
-    return value;
-}
-
 constexpr auto parse_int_unsafe(std::string_view str) noexcept
     -> std::int64_t
 {
@@ -40,6 +21,13 @@ constexpr auto parse_int_unsafe(std::string_view str) noexcept
     }
 
     return value;
+}
+
+constexpr auto parse_double_unsafe(std::string_view str) noexcept
+    -> double
+{
+    // TODO: implement double parsing
+    return 0.0;
 }
 
 } // namespace parser
