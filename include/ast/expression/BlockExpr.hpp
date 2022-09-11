b864ef0fb6d94110562c560ef0c3d50f00ddca1e
Lukas <mail@lukas.contact>
2022-09-11 15:23:52 +0200

    make blockexpr constexpr

--- include/ast/expression/BlockExpr.hpp
+++ include/ast/expression/BlockExpr.hpp
@@ -10,25 +10,25 @@ namespace ast {
 class BlockExpr : public AreaBase
 {
 public:
-    BlockExpr(lexing::TextArea area, std::vector<Statement>&& body, Expression&& ret_expr) noexcept
+    constexpr BlockExpr(lexing::TextArea area, std::vector<Statement>&& body, Expression&& ret_expr) noexcept
         : AreaBase(area),
           body_(std::move(body)),
           ret_expr_(std::move(ret_expr)) {}
 
-    auto getBody() const noexcept -> const std::vector<Statement>&
+    constexpr auto getBody() const noexcept -> const std::vector<Statement>&
     {
         return body_;
     }
-    auto getBody() noexcept -> std::vector<Statement>&
+    constexpr auto getBody() noexcept -> std::vector<Statement>&
     {
         return body_;
     }
 
-    auto getReturnExpression() const noexcept -> const Expression&
+    constexpr auto getReturnExpression() const noexcept -> const Expression&
     {
         return ret_expr_;
     }
-    auto getReturnExpression() noexcept -> Expression&
+    constexpr auto getReturnExpression() noexcept -> Expression&
     {
         return ret_expr_;
     }
