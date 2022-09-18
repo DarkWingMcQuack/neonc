89740f2d1699a29024797f99e336a49544416486
Lukas <mail@lukas.contact>
2022-09-18 21:20:19 +0200

    make tuple expr constexpr

--- include/ast/expression/TupleExpr.hpp
+++ include/ast/expression/TupleExpr.hpp
@@ -1,23 +1,26 @@
 #pragma once
 
 #include <ast/Ast.hpp>
-#include <vector>
 #include <ast/common/AreaBase.hpp>
+#include <vector>
 
 namespace ast {
 
 class TupleExpr : public AreaBase
 {
 public:
-    TupleExpr(lexing::TextArea area, std::vector<Expression>&& exprs) noexcept
+    constexpr TupleExpr(lexing::TextArea area, std::vector<Expression>&& exprs) noexcept
         : AreaBase(area),
           expressions_(std::move(exprs)) {}
 
-    auto getExpressions() const noexcept -> const std::vector<Expression>&
+    constexpr TupleExpr(TupleExpr&&) noexcept = default;
+    constexpr auto operator=(TupleExpr&&) noexcept -> TupleExpr& = default;
+
+    constexpr auto getExpressions() const noexcept -> const std::vector<Expression>&
     {
         return expressions_;
     }
-    auto getExpressions() noexcept -> std::vector<Expression>&
+    constexpr auto getExpressions() noexcept -> std::vector<Expression>&
     {
         return expressions_;
     }
