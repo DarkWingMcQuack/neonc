95ab9c31a15c12b3d82dc2bf15808cfa0f73bbbb
Lukas <mail@lukas.contact>
2022-10-09 17:58:18 +0200

    added more tests regarding multiplication, substraction and addition

--- test/parser/ExpressionParserTest.cpp
+++ test/parser/ExpressionParserTest.cpp
@@ -6,12 +6,12 @@
 
 using parser::Parser;
 
-auto id(std::string_view text) -> ast::Identifier
+inline auto id(std::string_view text) -> ast::Identifier
 {
 	return ast::Identifier{{0, 0}, text};
 }
 
-auto add(auto lhs, auto rhs) -> ast::Expression
+inline auto add(auto lhs, auto rhs) -> ast::Expression
 {
 	return ast::Expression{
 		ast::forward<ast::Addition>(lexing::TextArea{0, 0},
@@ -19,7 +19,15 @@ auto add(auto lhs, auto rhs) -> ast::Expression
 									std::move(rhs))};
 }
 
-auto mul(auto lhs, auto rhs) -> ast::Expression
+inline auto sub(auto lhs, auto rhs) -> ast::Expression
+{
+	return ast::Expression{
+		ast::forward<ast::Substraction>(lexing::TextArea{0, 0},
+										std::move(lhs),
+										std::move(rhs))};
+}
+
+inline auto mul(auto lhs, auto rhs) -> ast::Expression
 {
 	return ast::Expression{
 		ast::forward<ast::Multiplication>(lexing::TextArea{0, 0},
@@ -36,9 +44,8 @@ auto expr_test_positive(std::string_view text, auto expected)
 }
 
 
-TEST(ExpressionParserTest, AdditionExpressionParsingTest)
+TEST(ExpressionParserTest, SimpleAdditionExpressionParsingTest)
 {
-
 	expr_test_positive("i + j", add(id("i"), id("j")));
 	expr_test_positive("i + j + k",
 					   add(
@@ -53,7 +60,97 @@ TEST(ExpressionParserTest, AdditionExpressionParsingTest)
 							   id("l")),
 						   id("m")));
 
+	expr_test_positive("i + (j + k)",
+					   add(id("i"),
+						   add(id("j"), id("k"))));
+
 	// expr_test_positive("i + j * k", add(id("i"), mul(id("j"), id("k"))));
 
 	// expr_test_positive("i + j + k", add(add(id("i"), id("j")), id("k")));
+}
+
+TEST(ExpressionParserTest, SimpleSubstractionExpressionParsingTest)
+{
+	expr_test_positive("i - j", sub(id("i"), id("j")));
+	expr_test_positive("i - j - k",
+					   sub(
+						   sub(id("i"), id("j")),
+						   id("k")));
+	expr_test_positive("i - j - k - l - m",
+					   sub(
+						   sub(
+							   sub(
+								   sub(id("i"), id("j")),
+								   id("k")),
+							   id("l")),
+						   id("m")));
+
+	expr_test_positive("i - (j - k)",
+					   sub(id("i"),
+						   sub(id("j"), id("k"))));
+}
+
+TEST(ExpressionParserTest, SimpleAdditionSubstractionExpressionParsingTest)
+{
+	expr_test_positive("i - j", sub(id("i"), id("j")));
+	expr_test_positive("i + j - k",
+					   sub(
+						   add(id("i"), id("j")),
+						   id("k")));
+	expr_test_positive("i + j - k + l + m",
+					   add(
+						   add(
+							   sub(
+								   add(id("i"), id("j")),
+								   id("k")),
+							   id("l")),
+						   id("m")));
+
+	expr_test_positive("i + (j - k)",
+					   add(id("i"),
+						   sub(id("j"), id("k"))));
+}
+
+
+TEST(ExpressionParserTest, SimpleMultiplicationExpressionParsingTest)
+{
+	expr_test_positive("i * j", mul(id("i"), id("j")));
+	expr_test_positive("i * j * k",
+					   mul(
+						   mul(id("i"), id("j")),
+						   id("k")));
+	expr_test_positive("i * j * k * l * m",
+					   mul(
+						   mul(
+							   mul(
+								   mul(id("i"), id("j")),
+								   id("k")),
+							   id("l")),
+						   id("m")));
+
+	expr_test_positive("i * (j * k)",
+					   mul(id("i"),
+						   mul(id("j"), id("k"))));
+}
+
+TEST(ExpressionParserTest, SimpleMultiplicationAdditionExpressionParsingTest)
+{
+	expr_test_positive("i * j", mul(id("i"), id("j")));
+	expr_test_positive("i + j * k",
+					   add(id("i"),
+						   mul(id("j"),
+							   id("k"))));
+	expr_test_positive("i + j * k * l + m",
+					   add(
+						   add(
+							   id("i"),
+							   mul(
+								   mul(id("j"),
+									   id("k")),
+								   id("l"))),
+						   id("m")));
+
+	expr_test_positive("i * (j + k)",
+					   mul(id("i"),
+						   add(id("j"), id("k"))));
 }
