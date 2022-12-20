46764c2e8debe5bd52083110a772249d2b2d0549
Lukas <mail@lukas.contact>
2022-12-21 00:11:02 +0100

    added some error types

---
+++ include/common/Error.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <lexer/TextArea.hpp>
+#include <lexer/Tokens.hpp>
+#include <variant>
+#include <vector>
+
+namespace common::error {
+
+class UnknownToken
+{
+public:
+    constexpr UnknownToken(lexing::TextArea area) noexcept
+        : area_(std::move(area)) {}
+
+private:
+    lexing::TextArea area_;
+};
+
+class UnclosedString
+{
+public:
+    constexpr UnclosedString(lexing::TextArea area) noexcept
+        : area_(std::move(area)) {}
+
+private:
+    lexing::TextArea area_;
+};
+
+class UnexpectedToken
+{
+    template<class... Exp>
+        requires(sizeof...(Exp) >= 1) and (std::same_as<Exp, lexing::TokenTypes> and ...)
+    constexpr UnexpectedToken(lexing::TokenTypes actual,
+                              lexing::TextArea area,
+                              Exp... expected) noexcept
+        : area_(std::move(area)),
+          actual_(std::move(actual)),
+          expected_{std::move(expected)...}
+    {}
+
+private:
+    lexing::TextArea area_;
+    lexing::TokenTypes actual_;
+    std::vector<lexing::TokenTypes> expected_;
+};
+
+using Error = std::variant<UnknownToken, UnclosedString, UnexpectedToken>;
+
+} // namespace common::error
