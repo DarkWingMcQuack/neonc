888268f943ddcb22a58dfc0640b1f2e93571afb0
Lukas <mail@lukas.contact>
2022-12-21 00:11:34 +0100

    switch to tl::expected

--- cmake/expected.cmake
+++ cmake/expected.cmake
@@ -5,15 +5,14 @@ set(CMAKE_ARGS
   -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
   -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
   -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
-  -DEXPECTED_LITE_OPT_BUILD_TESTS=OFF
-  -DEXPECTED_LITE_OPT_BUILD_EXAMPLES=OFF)
+  -DEXPECTED_OPT_BUILD_TESTS=OFF
+  -DEXPECTED_OPT_BUILD_EXAMPLES=OFF)
 
-ExternalProject_Add(expected-lite-project
-  PREFIX deps/expected-lite
+ExternalProject_Add(expected-project
+  PREFIX deps/expected
   DOWNLOAD_DIR ${CMAKE_BINARY_DIR}/downloads
-  GIT_REPOSITORY https://github.com/martinmoene/expected-lite
+  GIT_REPOSITORY https://github.com/TartanLlama/expected
   CMAKE_ARGS ${CMAKE_ARGS}
-  PATCH_COMMAND cmake -E make_directory <SOURCE_DIR>/win32-deps/include
   BUILD_COMMAND cmake --build <BINARY_DIR> --config Release
   UPDATE_COMMAND ""
   INSTALL_COMMAND cmake --build <BINARY_DIR> --config Release --target install
@@ -21,8 +20,8 @@ ExternalProject_Add(expected-lite-project
   DOWNLOAD_EXTRACT_TIMESTAMP TRUE
   )
 
-ExternalProject_Get_Property(expected-lite-project INSTALL_DIR)
-set(EXPECTED_LITE_INCLUDE_DIR ${INSTALL_DIR}/include)
+ExternalProject_Get_Property(expected-project INSTALL_DIR)
+set(EXPECTED_INCLUDE_DIR ${INSTALL_DIR}/include)
 
 unset(INSTALL_DIR)
 unset(CMAKE_ARGS)
