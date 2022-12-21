e07927e8f1a2509bb53879bc454187e068a60491
Lukas <mail@lukas.contact>
2022-12-21 18:00:19 +0100

    enable sanitizers for tests

--- cmake/flags.cmake
+++ cmake/flags.cmake
@@ -1,26 +1,26 @@
 function(setup_linker target)
   if(${CMAKE_CXX_COMPILER_ID} MATCHES Clang)
-	find_program(LLD lld)
-	if(LLD)
-	  message(STATUS "found lld, using it as linker")
-	  target_link_options(${target} PUBLIC -fuse-ld=lld)
-	elseif(CMAKE_BUILD_TYPE MATCHES "Release")
-	  message(FATAL_ERROR "Gold linker must be installed for building in Release mode.")
-	else()
-	  message(STATUS "LLD not available, using the system linker for target ${target}")
-	endif()
+    find_program(LLD lld)
+    if(LLD)
+      message(STATUS "found lld, using it as linker")
+      target_link_options(${target} PUBLIC -fuse-ld=lld)
+    elseif(CMAKE_BUILD_TYPE MATCHES "Release")
+      message(FATAL_ERROR "Gold linker must be installed for building in Release mode.")
+    else()
+      message(STATUS "LLD not available, using the system linker for target ${target}")
+    endif()
   endif()
 
   if(${CMAKE_CXX_COMPILER_ID} MATCHES GNU)
     find_program(GNU_GOLD gold)
     if(GNU_GOLD)
-	  message(STATUS "found GNU gold linker, using it as linker for target ${target}")
-	  target_link_options(${target} PUBLIC -fuse-ld=gold;LINKER:--threads,--thread-count=${HOST_PROC_COUNT})
-	elseif(CMAKE_BUILD_TYPE MATCHES "Release")
-	  message(FATAL_ERROR "Gold linker must be installed for building in Release mode.")
-	else()
-	  message(STATUS "Gold not available, using the system linker for target ${target}")
-	endif()
+      message(STATUS "found GNU gold linker, using it as linker for target ${target}")
+      target_link_options(${target} PUBLIC -fuse-ld=gold;LINKER:--threads,--thread-count=${HOST_PROC_COUNT})
+    elseif(CMAKE_BUILD_TYPE MATCHES "Release")
+      message(FATAL_ERROR "Gold linker must be installed for building in Release mode.")
+    else()
+      message(STATUS "Gold not available, using the system linker for target ${target}")
+    endif()
   endif()
 endfunction()
 
@@ -32,7 +32,7 @@ function(set_flags target)
     $<$<CONFIG:DEBUG>:-g3>
     $<$<CXX_COMPILER_ID:Clang>:-ferror-limit=2>
     $<$<CONFIG:DEBUG>:-fno-optimize-sibling-calls>
-	$<$<CONFIG:DEBUG>:-fno-omit-frame-pointer>
+    $<$<CONFIG:DEBUG>:-fno-omit-frame-pointer>
     $<$<CONFIG:DEBUG>:-Wfloat-equal>
     $<$<CONFIG:DEBUG>:-Wpointer-arith>
     $<$<CONFIG:DEBUG>:-Wformat-nonliteral>
@@ -45,5 +45,23 @@ function(set_flags target)
     $<$<AND:$<CXX_COMPILER_ID:GNU>,$<CONFIG:RELEASE>>:-fdevirtualize-at-ltrans>
     $<$<AND:$<CXX_COMPILER_ID:GNU>,$<CONFIG:RELEASE>>:-fdevirtualize-speculatively>
   )
-
 endfunction()
+
+function(set_sanitizers target)
+  target_compile_options(${target} PRIVATE
+    -fsanitize=address
+    -fsanitize=leak
+    -fsanitize=undefined
+    -fsanitize=pointer-subtract
+    -fsanitize=pointer-compare
+    )
+
+  target_link_options(${target} PRIVATE
+    -fsanitize=address
+    -fsanitize=leak
+    -fsanitize=undefined
+    -fsanitize=pointer-subtract
+    -fsanitize=pointer-compare
+    )
+endfunction()
+
