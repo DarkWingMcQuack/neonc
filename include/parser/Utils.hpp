4052f4cff7104fc6a1e72cc963054702d648b45e
Lukas <mail@lukas.contact>
2023-06-01 00:04:59 +0200

    added a simple double parsing

--- include/parser/Utils.hpp
+++ include/parser/Utils.hpp
@@ -26,8 +26,49 @@ constexpr auto parse_int_unsafe(std::string_view str) noexcept
 constexpr auto parse_double_unsafe(std::string_view str) noexcept
     -> double
 {
-    // TODO: implement double parsing
-    return 0.0;
+    double result = 0.0;
+    std::size_t index = 0;
+
+    // Parse integer part
+    while(index < str.size() && std::isdigit(str[index])) {
+        result = result * 10 + (str[index++] - '0');
+    }
+
+    if(index < str.size() && str[index] == '.') {
+        double fraction = 1.0;
+        ++index;
+
+        // Parse fractional part
+        while(index < str.size() && std::isdigit(str[index])) {
+            fraction /= 10.0;
+            result += (str[index++] - '0') * fraction;
+        }
+    }
+
+    if(index < str.size() && (str[index] == 'e' || str[index] == 'E')) {
+        ++index;
+
+        double base = 10.0;
+        if(index < str.size()) {
+            if(str[index] == '-') {
+                base = 0.1;
+                ++index;
+            } else if(str[index] == '+') {
+                ++index;
+            }
+        }
+
+        unsigned exponent = 0;
+        while(index < str.size() && std::isdigit(str[index])) {
+            exponent = exponent * 10 + (str[index++] - '0');
+        }
+
+        while(exponent--) {
+            result *= base;
+        }
+    }
+
+    return result;
 }
 
 } // namespace parser
