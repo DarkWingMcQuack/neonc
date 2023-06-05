b1d5861730e6b94d1eaeb3815731fbbee70e5b5b
Lukas <mail@lukas.contact>
2023-06-05 16:19:05 +0200

    added documentation

--- include/ast/expression/BlockExpr.hpp
+++ include/ast/expression/BlockExpr.hpp
@@ -7,9 +7,26 @@
 
 namespace ast {
 
+/**
+ * @class BlockExpr
+ * @brief Represents a block of code in the Abstract Syntax Tree for the Neon programming language.
+ * @details This class stores a vector of statements that make up the body of a block, along with a return expression.
+ * @example
+ * {
+ *   let a = 10;
+ *   let b = a * a;
+ *   => b * 2
+ * }
+ */
 class BlockExpr : public AreaBase
 {
 public:
+    /**
+     * @brief Constructor that sets the area, body, and return expression of the BlockExpr.
+     * @param area Represents the area where the block is defined.
+     * @param body Represents the statements that form the body of the block.
+     * @param ret_expr Represents the return expression of the block.
+     */
     constexpr BlockExpr(lexing::TextArea area,
                         std::vector<Statement>&& body,
                         Expression&& ret_expr) noexcept
@@ -17,38 +34,66 @@ public:
           body_(std::move(body)),
           ret_expr_(std::move(ret_expr)) {}
 
+    // Deleting the default constructor and the copy constructor
     constexpr BlockExpr() noexcept = delete;
     constexpr BlockExpr(const BlockExpr&) noexcept = delete;
+
+    // Providing the default move constructor and move assignment operator
     constexpr BlockExpr(BlockExpr&&) noexcept = default;
     constexpr auto operator=(BlockExpr&&) noexcept -> BlockExpr& = default;
+
+    // Deleting the copy assignment operator to prevent
     constexpr auto operator=(const BlockExpr&) noexcept -> BlockExpr& = delete;
 
+    /**
+     * @brief Equality comparison operator. Checks if two BlockExpr objects are equal.
+     * @param other The BlockExpr object to compare with.
+     * @return true if the BlockExpr objects are equal, false otherwise.
+     */
     constexpr auto operator==(const BlockExpr& other) const noexcept -> bool
     {
         return body_ == other.body_ and ret_expr_ == other.ret_expr_;
     }
 
+    /**
+     * @brief Returns a const reference to the vector of statements that form the body of the block.
+     * @return const reference to body_.
+     */
     constexpr auto getBody() const noexcept -> const std::vector<Statement>&
     {
         return body_;
     }
+
+    /**
+     * @brief Returns a non-const reference to the vector of statements that form the body of the block.
+     * @return non-const reference to body_.
+     */
     constexpr auto getBody() noexcept -> std::vector<Statement>&
     {
         return body_;
     }
 
+    /**
+     * @brief Returns a const reference to the return expression of the block.
+     * @return const reference to ret_expr_.
+     */
     constexpr auto getReturnExpression() const noexcept -> const Expression&
     {
         return ret_expr_;
     }
+
+    /**
+     * @brief Returns a non-const reference to the return expression of the block.
+     * @return non-const reference to ret_expr_.
+     */
     constexpr auto getReturnExpression() noexcept -> Expression&
     {
         return ret_expr_;
     }
 
 private:
-    std::vector<Statement> body_;
-    Expression ret_expr_;
+    std::vector<Statement> body_; ///< Vector of statements that form the body of the block
+    Expression ret_expr_; ///< Return expression of the block
 };
 
 } // namespace ast
--- include/parser/types/BasicTypeParser.hpp
+++ include/parser/types/BasicTypeParser.hpp
@@ -14,10 +14,22 @@
 
 namespace parser {
 
+/**
+ * @class BasicTypeParser
+ * @brief BasicTypeParser is a template class for parsing basic types in a given language, following the CRTP (Curiously Recurring Template Pattern).
+ * @details The class provides the basic functionality for parsing types and expects the derived class to provide implementations for par_type(), self_type(), named_type(), and the lexer_ member.
+ */
 template<class T>
 class BasicTypeParser
 {
 public:
+    /**
+     * @brief Tries to parse a basic type.
+     * @details Tries to parse either a parenthesized type, a self type, or a named type, based on the next token in the lexer.
+     *          If none of these is possible, an UnexpectedToken error is returned.
+     * @return On success, returns an expected value containing the parsed Type.
+     *         On failure, returns an expected value containing an UnexpectedToken error.
+     */
     constexpr auto basic_type() noexcept -> std::expected<ast::Type, common::error::Error>
     {
         using common::error::UnexpectedToken;
@@ -51,6 +63,11 @@ public:
     }
 
 private:
+    /**
+     * @brief Accesses the lexer.
+     * @details Returns a reference to the lexer that is being used by the parser. The lexer is expected to be provided by the derived class.
+     * @return Reference to the lexer.
+     */
     constexpr auto basic_type_lexer() noexcept -> lexing::Lexer &
     {
         return static_cast<T *>(this)->lexer_;
